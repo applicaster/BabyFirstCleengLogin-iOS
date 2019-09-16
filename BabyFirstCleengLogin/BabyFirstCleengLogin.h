9e0756965daf91dec37632dd9c848b68c3b99b3d
Roi <roiked@gmail.com>
2019-09-16 15:42:51 +0300

    add project

---
+++ BabyFirstCleengLogin/BabyFirstCleengLogin.h
@@ -0,0 +1,19 @@
+//
+//  BabyFirstCleengLogin.h
+//  BabyFirstCleengLogin
+//
+//  Created by Roi Kedarya on 16/09/2019.
+//  Copyright © 2019 applicaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BabyFirstCleengLogin.
+FOUNDATION_EXPORT double BabyFirstCleengLoginVersionNumber;
+
+//! Project version string for BabyFirstCleengLogin.
+FOUNDATION_EXPORT const unsigned char BabyFirstCleengLoginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BabyFirstCleengLogin/PublicHeader.h>
+
+
